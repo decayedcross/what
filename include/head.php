d4242f725af27e1885878e18f44c5e54b57b7547
decayedcross <decayedcross@gmail.com>
2015-06-02 14:14:51 -0700

    added meta tags

--- include/head.php
+++ include/head.php
@@ -21,10 +21,6 @@ $articleCheckedOutTime = $article->get('checked_out_time');
 $articlePublishUp = $article->get('publish_up');
 $articlePublishDown = $article->get('publish_down');
 
-$articleImages = json_decode($article->get('images'));
-
-$articleUrls = $article->get('urls');
-
 $articleAttribs = $article->get('attribs');
 $articleVersion = $article->get('version');
 $articleOrdering = $article->get('ordering');
@@ -32,11 +28,11 @@ $articleMetaKey = $article->get('metakey');
 $articleMetaDesc = $article->get('metadesc');
 $articleAccess = $article->get('access');
 $articleHits = $article->get('hits');
-$articleMetaData = $article->get('metadata');
 $articleFeatured = $article->get('featured');
 $articleLanguage = $article->get('language');
 $articleXrefence = $article->get('xreference');
 
+$articleImages = json_decode($article->get('images'));
 $articleImageIntro = $articleImages->image_intro;
 $articleImageFloatIntro = $articleImages->float_intro;
 $articleImageIntroAlt = $articleImages->image_intro_alt;
@@ -45,18 +41,43 @@ $articleImageFulltext = $articleImages->image_fulltext;
 $articleImageFloatFulltext = $articleImages->float_fulltext;
 $articleImageFulltextAlt = $articleImages->image_fulltext_alt;
 $articleImageFulltextCaption = $articleImages->image_fulltext_caption;
+
+$articleUrls = $article->get('urls');
+
+
+$articleMetaData = json_decode($article->get('metadata'));
+$articleMetaDataRobots = $articleMetaData->robots;
+$articleMetaDataAuthor = $articleMetaData->author;
+$articleMetaDataRights = $articleMetaData->rights;
+$articleMetaDataXreference = $articleMetaData->xreference;
+
+$config = JFactory::getConfig();
+$globalMailFrom = $config->get('mailfrom');
+$globalSiteName = $config->get('sitename');
 ?>
 
 
 <meta charset="utf-8">
+<meta name='keywords' content='<?php echo $articleMetaKey; ?>'>
+<meta name='language' content='<?php echo $articleLanguage; ?>'>
+<meta name='robots' content='<?php echo $articleMetaDataRobots; ?>'>
+<meta name='revised' content='<?php echo $articleModified; ?>'>
+<meta name='author' content='<?php echo $articleMetaDataAuthor; ?>'>
+<meta name='designer' content='Viking Web Development'>
+<meta name='reply-to' content='<?php echo $globalMailFrom; ?>'>
+<meta name='pagename' content='<?php echo $articleTitle; ?>'>
 <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+<meta name='HandheldFriendly' content='True'>
+<meta name='MobileOptimized' content='240'>
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="<?php echo $articleMetaDesc; ?>">
 <title><?php echo $articleTitle; ?></title>
 <link href="templates/what/css/load.css" type="text/css" rel="stylesheet">
 
 <!-- FACEBOOK -->
-<meta property="og:url" content="<?php echo 'http://'.$_SERVER[HTTP_HOST].$_SERVER[REQUEST_URI]; ?>" />
+<meta property="og:url" content="<?php echo JUri::getInstance(); ?>" />
 <meta property="og:type" content="website" />
 <meta property="og:title" content="<?php echo $articleTitle; ?>" />
 <meta property="og:description" content="<?php echo $articleMetaDesc; ?>" />
 <meta property="og:image" content="<?php echo $articleImageIntro; ?>" />
+<meta property='og:site_name' content='<?php echo $globalSiteName; ?>'>
